98c8c1e5277f27c7f97d9b244983068d195f43a1
IanW000 <90727502+IanW000@users.noreply.github.com>
2021-10-04 13:22:40 -0400

    Updated appearance of UI prefab
    And now level selecting page is connecting to our final gameplay scenes

--- Assets/Scripts/UI/LevelSelecting.cs
+++ Assets/Scripts/UI/LevelSelecting.cs
@@ -8,23 +8,23 @@ public class LevelSelecting : MonoBehaviour
 {
     public void Level1()
     {
-        SceneManager.LoadScene("IanTestMainScene");
+        SceneManager.LoadScene(2);
 
     }
     public void Level2()
     {
-        Debug.Log("level2");
+        SceneManager.LoadScene(3);
     }
     public void Level3()
     {
-        Debug.Log("level3");
+        SceneManager.LoadScene(4);
     }
     public void Level4()
     {
-        Debug.Log("level4");
+        SceneManager.LoadScene(5);
     }
     public void Level5()
     {
-        Debug.Log("level5");
+        SceneManager.LoadScene(6);
     }
 }
