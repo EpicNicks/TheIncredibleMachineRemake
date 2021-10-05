c018f787676764031aeafd209a000229fb04e5b7
Lukas-Rad <90727504+Lukas-Rad@users.noreply.github.com>
2021-10-05 13:42:00 -0400

    added sound to elevator

--- Assets/LevelObjects/Prefabs/Prefab Scripts/ElevatorPlace.cs
+++ Assets/LevelObjects/Prefabs/Prefab Scripts/ElevatorPlace.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class ElevatorPlace : Placeable
 {
+
+    public AudioClip elevatorSound;
+
     // Start is called before the first frame update
     private void Start() {
         base.OnStart();
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = elevatorSound;
     }
 
     private void LateUpdate() {
         base.OnLateUpdate();
+    }
+
+    private void onCollision()
+    {
+        GetComponent<AudioSource>().Play();
     }
 }
