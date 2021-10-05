3723ae79dbc3f74205d0799ae6bb2b85f55b4a75
Jason <hjhawc96@my.yorku.ca>
2021-10-05 02:01:24 -0400

    Level1

---
+++ Assets/LevelObjects/Prefabs/Prefab Scripts/ElevatorPlace.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPlace : Placeable
+{
+    // Start is called before the first frame update
+    private void Start() {
+        base.OnStart();
+    }
+
+    private void LateUpdate() {
+        base.OnLateUpdate();
+    }
+}
