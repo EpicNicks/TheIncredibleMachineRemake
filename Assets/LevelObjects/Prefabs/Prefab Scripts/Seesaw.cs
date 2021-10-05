7cd06a76bb84735f4d9f299994de8d1171ece906
Jason <hjhawc96@my.yorku.ca>
2021-10-05 10:52:06 -0400

    all levels done

--- Assets/LevelObjects/Prefabs/Prefab Scripts/Seesaw.cs
+++ Assets/LevelObjects/Prefabs/Prefab Scripts/Seesaw.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Seesaw : Placeable
+public class Seesaw : Placeable 
 {
     private void Start() {
         base.OnStart();
